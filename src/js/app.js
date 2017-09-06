3e2e4488c9c349061e07884bb7032d92a990e281
cfg111sleepy <ihorsleepy@gmail.com>
2017-09-06 19:11:24 +0300

    [+] show github repos

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,11 @@
 jQuery(document).ready(function ($) {
+	$.getJSON('https://api.github.com/search/repositories?q=bootstrap-sass&sort=stars&order=desc')
+		.then(function(data) {
+			var html = '';
+			data.items.forEach(function(item){
+				html += '<p>' + item.name + '</p>';
+			});
+			$('#app').html(html);
+		});
 	console.log('DOM Ready');
 });
